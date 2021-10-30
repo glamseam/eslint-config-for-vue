41bae6ed43dee9d766e6e7a02f28d5b08a71ad1e
Daisuke Fujimoto <glamseam@gmail.com>
2021-10-31 00:05:16 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const index = {
+export = {
     extends: [
-        'plugin:vue/vue3-essential',
-        'plugin:vue/vue3-strongly-recommended',
+        // 'plugin:vue/vue3-essential',
+        // 'plugin:vue/vue3-strongly-recommended',
         'plugin:vue/vue3-recommended'
     ],
     globals: {
@@ -50,5 +50,3 @@ const index = {
         'vue/no-v-html': ['warn']
     }
 }
-
-export = index
