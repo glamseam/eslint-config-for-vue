07a4d83e86628baf48fafceb51faf4a3146d4088
glamseam <glamseam@gmail.com>
2024-03-23 13:56:04 +0900

    fix: Rules

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 export = {
-    extends: [
-        // 'plugin:vue/vue3-essential',
-        // 'plugin:vue/vue3-strongly-recommended',
-        'plugin:vue/vue3-recommended'
-    ],
+    extends: ['plugin:vue/vue3-recommended'],
     parser: 'vue-eslint-parser',
     parserOptions: {
+        project: true,
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
         extraFileExtensions: ['.vue'],
@@ -21,25 +18,26 @@ export = {
     },
     rules: {
         'vue/html-indent': [
-            'error', 4,
+            'error',
+            4,
             {
-                'attribute': 1,
-                'baseIndent': 1,
-                'closeBracket': 0,
-                'alignAttributesVertically': true,
-                'ignores': []
+                attribute: 1,
+                baseIndent: 1,
+                closeBracket: 0,
+                alignAttributesVertically: true,
+                ignores: []
             }
         ],
         'vue/html-self-closing': [
             'error',
             {
-                'html': {
-                    'void': 'never',
-                    'normal': 'always',
-                    'component': 'always'
+                html: {
+                    void: 'never',
+                    normal: 'always',
+                    component: 'always'
                 },
-                'svg': 'always',
-                'math': 'always'
+                svg: 'always',
+                math: 'always'
             }
         ],
         'vue/return-in-computed-property': 'off',
@@ -49,11 +47,7 @@ export = {
         'vue/component-tags-order': [
             'error',
             {
-                'order': [
-                    'template',
-                    'script',
-                    'style'
-                ]
+                order: ['template', 'script', 'style']
             }
         ],
         'vue/no-v-html': ['warn'],
@@ -61,10 +55,7 @@ export = {
         'vue/no-undef-components': [
             'error',
             {
-                'ignorePatterns': [
-                    'RouterLink',
-                    'RouterView'
-                ]
+                ignorePatterns: ['RouterLink', 'RouterView']
             }
         ],
         'vue/no-v-text-v-html-on-component': ['error']
